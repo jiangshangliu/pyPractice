d6c383d02ad0cb143a411d6f08b510f575b24f2c
jiangshangliu <649867415@qq.com>
2019-03-04 01:08:54 +0800

    two commit

---
+++ New01/p01.py
@@ -0,0 +1,16 @@
+# print('Hello world')
+# def pt():
+#     for i in 'helloworld':
+#         print(i)
+#
+# print('this is my first project of the new computer')
+
+# with open("first.txt",'a') as f:
+#     f.write("毛衣女孩真漂亮")
+# print('结束')
+
+# with open('C:/Users/刘小川/Pictures/Camera Roll/可爱与性感.jpg','rb') as p:
+#     con = p.read()
+# with open('C:/Users/刘小川/Desktop/xico.jpg','wb') as m:
+#     m.write(con)
+# print('成功！')
